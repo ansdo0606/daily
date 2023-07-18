975ae5f0b23372a21ed07507f1d195db59a4c7c1
ansdo0606 <ansdo0606@gmail.com>
2023-07-18 12:03:19 +0900

    programmers
    Day 1

---
+++ Day01/src/com/moon/main/Sol05.java
@@ -0,0 +1,19 @@
+/**
+ * 특수문자 출력하기
+	문제 설명
+	다음과 같이 출력하도록 코드를 작성해 주세요.
+	
+	출력 예시
+	
+	!@#$%^&*(\'"<>?:;
+ */
+package com.moon.main;
+
+public class Sol05 {
+
+	public static void main(String[] args) {
+		String specialCharacters = "!@#$%^&*(\\'\"<>?:;";
+        System.out.println(specialCharacters);
+	}
+
+}
